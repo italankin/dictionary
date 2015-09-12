fad5325d90efe4a8e7512b8445b991aa8ce1c034
italankin <italankin@gmail.com>
2015-09-12 06:05:17 +0500

    Resource updates

--- app/src/main/java/ga/italankin/translate/TranslateActivity.java
+++ app/src/main/java/ga/italankin/translate/TranslateActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -56,6 +57,13 @@ public class TranslateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translate);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setLogo(R.mipmap.ic_launcher);
+            actionBar.setDisplayUseLogoEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
 
         mPrefs = getPreferences(MODE_PRIVATE);
         mClipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
