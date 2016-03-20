3d728006d9eee6a16155b4ede1a34fcca41544f2
italankin <italankin@gmail.com>
2016-03-20 22:24:15 +0500

    Added licensing and stuff.

--- app/src/main/java/com/italankin/dictionary/ui/SettingsActivity.java
+++ app/src/main/java/com/italankin/dictionary/ui/SettingsActivity.java
@@ -1,5 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.italankin.dictionary.ui;
-
 
 import android.app.Fragment;
 import android.app.FragmentManager;
@@ -12,13 +24,13 @@ import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
 import com.italankin.dictionary.R;
 import com.italankin.dictionary.utils.SharedPrefs;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.psdev.licensesdialog.LicensesDialog;
 
 /**
  * Activity for displaying and manipulating user preferences.
@@ -76,7 +88,11 @@ public class SettingsActivity extends AppCompatActivity {
         public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
             switch (preference.getKey()) {
                 case "open_source_libs":
-                    Toast.makeText(getActivity(), "TODO", Toast.LENGTH_SHORT).show(); // TODO
+                    LicensesDialog dialog = new LicensesDialog.Builder(getActivity())
+                            .setNotices(R.raw.notices)
+                            .setTitle(R.string.pref_open_source_libs)
+                            .build();
+                    dialog.showAppCompat();
                     return true;
             }
             return false;
